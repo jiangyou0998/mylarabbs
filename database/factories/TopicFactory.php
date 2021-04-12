81d20d7474372b93f9cbf4e8df23410c1d090508
jianl <fs378354476@outlook.com>
2021-04-12 05:06:35 +0000

    话题排序

--- database/factories/TopicFactory.php
+++ database/factories/TopicFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use App\Models\Topic;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class TopicFactory extends Factory
@@ -12,6 +13,10 @@ class TopicFactory extends Factory
     public function definition()
     {
         $sentence = $this->faker->sentence();
+        $time_sub = $this->faker->randomElement([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+        $created_at = Carbon::now()->subDay($time_sub);
+        $time_add = $this->faker->randomElement([0,1]);
+        $updated_at = Carbon::now()->subDay($time_sub)->addDay($time_add);
 
         return [
             'title' => $sentence,
@@ -19,6 +24,9 @@ class TopicFactory extends Factory
             'excerpt' => $sentence,
             'user_id' => $this->faker->randomElement([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),
             'category_id' => $this->faker->randomElement([1, 2, 3, 4]),
+            'created_at' => $created_at,
+            'updated_at' => $updated_at,
+
         ];
     }
 }
