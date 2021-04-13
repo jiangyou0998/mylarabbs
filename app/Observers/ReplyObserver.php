86a0566c5023c8b09cdf2e5b114661f6074497bd
jianl <fs378354476@outlook.com>
2021-04-13 07:49:17 +0000

    消息通知有新回复

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -3,9 +3,7 @@
 namespace App\Observers;
 
 use App\Models\Reply;
-
-// creating, created, updating, updated, saving,
-// saved,  deleting, deleted, restoring, restored
+use App\Notifications\TopicReplied;
 
 class ReplyObserver
 {
@@ -13,8 +11,10 @@ class ReplyObserver
     {
         $reply->topic->reply_count = $reply->topic->replies->count();
         $reply->topic->save();
-    }
 
+        // 通知话题作者有新的评论
+        $reply->topic->user->notify(new TopicReplied($reply));
+    }
     public function creating(Reply $reply)
     {
         $reply->content = clean($reply->content, 'user_topic_body');
